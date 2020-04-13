4e61c3894729a0e8c1f98886f59abbdca62f59b7
desa007 <desa.odessa@gmail.com>
2020-04-13 19:37:57 +0300

    first commit

---
+++ src/MaxmindAdapter.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Hillel\Library;
+
+
+class MaxMindAdapter
+{
+    protected $reader;
+    protected $record = 0;
+
+    public function __construct(\GeoIp2\Database\Reader $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    public function parse(string $ip)
+    {
+        try {
+            $this->record = $record = $this->reader->city($ip);
+        } catch (\GeoIp2\Exception\AddressNotFoundException $exception) {
+            $this->record = $record = $this->reader->city(env('DEFAULT_IP_ADDR'));
+        }
+    }
+
+    public function getCountryCode()
+    {
+        return $this->record->country->isoCode;
+    }
+
+    public function getCityName()
+    {
+        return $this->record->city->name;
+    }
+}
